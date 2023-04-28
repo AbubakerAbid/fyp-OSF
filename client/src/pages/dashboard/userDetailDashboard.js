e9a54053c9c5a810faa00d75be51569949f3a201
Abubaker-Abid <abubakerabid.com@gmail.com>
2023-04-28 16:02:49 +0500

    Height pus new

--- client/src/pages/dashboard/userDetailDashboard.js
+++ client/src/pages/dashboard/userDetailDashboard.js
@@ -39,7 +39,7 @@ const UserDetailDashboard = () =>{
     return (
         <>
        <section style={{display:'flex'}}>
-        <div style={{width:'20%',backgroundColor:'#1F253F', height:'100vh'}}>
+        <div style={{width:'20%',backgroundColor:'#1F253F'}}>
         <h3 style={{textAlign: 'center',fontSize: '24px',color: 'white',padding: '20px', borderBottom: '1px solid'}}>ADMIN</h3>
         <button onClick={workerDetailPage} style={{backgroundColor:'transparent', border:'0px', width: '100%'}}><h3 style={{textAlign: 'center',fontSize: '16px',color: 'white',padding: '20px', borderBottom: '1px solid', fontWeight:'400'}}>View Workers</h3></button>
         <button onClick ={approveWorkerPage} style={{backgroundColor:'transparent', border:'0px', width: '100%'}}><h3 style={{textAlign: 'center',fontSize: '16px',color: 'white',padding: '20px', borderBottom: '1px solid', fontWeight:'400'}}>Approve Worker</h3></button>
@@ -48,7 +48,7 @@ const UserDetailDashboard = () =>{
         <button onClick={logout} style={{backgroundColor:'transparent', border:'0px', width: '100%'}}><h3 style={{textAlign: 'center',fontSize: '16px',color: 'white',padding: '20px', borderBottom: '1px solid', fontWeight:'400'}}>Logout</h3></button>
        
         </div>
-        <div style={{width:'80%',backgroundColor:'rgb(156 170 192 / 33%)', height:'100vh'}}>
+        <div style={{width:'80%',backgroundColor:'rgb(156 170 192 / 33%)'}}>
         <h3 style={{textAlign: 'center',fontSize: '24px',color: '#1F253F',padding: '20px', borderBottom: '1px solid #1F253F'}}>Dashboard</h3>
         {users.map((user) => (
         <TableUser user = {user}/>
